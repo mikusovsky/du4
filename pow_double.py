f944e5e132c65cfea3497f2b4f3fc661dd390149
mikusovsky <a.mikusovsky@gmail.com>
2014-06-12 22:03:37 +0200

    add mpow functioun and remove read file

--- pow_double.py
+++ pow_double.py
@@ -1,7 +1,24 @@
 def pow(i):
     return i*i
 
-file = open('test.in', 'r')
+def mpow(i,k):
+    res = 1;
+    for j in range(k):
+        res *= i
+    return res
 
-for line in file:
-    print(pow(int(line)))
+exit = 1
+while exit:
+    fu = input('Please enter a function:')
+    in1 = 1
+    in2 = 1
+    if fu == "pow":
+        in1 = input('get value:')
+        print(pow(int(in1)))
+    elif fu == "mpow":
+        in1 = input('get value i:')
+        in2 = input('get value k:')
+        print(mpow(int(in1),int(in2)))
+    else:
+        exit = 0
+    
