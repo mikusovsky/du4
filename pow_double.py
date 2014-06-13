d646b0e736a1fcf62ea1642ca4c4651eb6341a0b
mikusovsky <a.mikusovsky@gmail.com>
2014-06-13 18:17:23 +0200

    add efective power function

--- pow_double.py
+++ pow_double.py
@@ -8,10 +8,23 @@ def mpow(i,k):
         res *= i
     return res
 
+def empow(x,n):
+        if n==0:
+            return 1
+        elif n==1:
+            return x
+        t = empow(x,n/2)
+        t*=t;
+        if n%2==1:
+            t*=x
+        return t
+
 print(pow(2))
 print(mpow(2,2))
+print(empow(2,2))
 print(pow(3))
 print(mpow(3,2))
+print(empow(3,2))
 
 
 exit = 1
@@ -21,6 +34,10 @@ while exit:
         in1 = input('get value:')
         print(pow(int(in1)))
     elif fu == "mpow":
+        in1 = input('get value i:')
+        in2 = input('get value k:')
+        print(mpow(int(in1),int(in2)))
+    elif fu == "empow":
         in1 = input('get value i:')
         in2 = input('get value k:')
         print(mpow(int(in1),int(in2)))
